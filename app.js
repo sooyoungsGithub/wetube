72659550898924574222ac4caf8c094bd0472751
devs <sooyoungs0120@gmail.com>
2019-05-14 23:50:35 +0900

    Add controllers && install pug

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import {
-    userRouter
-} from "./router";
-
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes"
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from home");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -21,11 +18,9 @@ app.use(bodyParser.urlencoded({
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
 // get이 아닌 use면 userRouter에 있는 모든 url을 쓰겠다.
-app.use("/user", userRouter)
+app.use(routes.home, globalRouter)
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter)
 
 export default app;
--- router.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-export const userRouter = express.Router();
-
-userRouter.get("/", (req, res) => res.send("userIndex"));
-userRouter.get("/edit", (req, res) => res.send("userEdit"));
-userRouter.get("/password", (req, res) => res.send("userPassword"));
