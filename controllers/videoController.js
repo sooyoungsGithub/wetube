7b97729a6b8eb51880ad649d191b4436e694c3fd
devs <sooyoungs0120@gmail.com>
2019-05-17 13:19:11 +0900

    Add mixins about videos

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,13 @@
-export const home = (req, res) => res.render("home", {
-    pageTitle: "Home"
-});
+import {
+    videos
+} from "../db";
+export const home = (req, res) => {
+    res.render("home", {
+        pageTitle: "Home",
+        videos
+    });
+};
+
 export const search = (req, res) => {
     const {
         query: {
@@ -9,21 +16,23 @@ export const search = (req, res) => {
     } = req;
     res.render("search", {
         pageTitle: "Search",
-        searchingBy
+        searchingBy,
+        videos
     });
 };
-export const videos = (req, res) => res.render("videos", {
-    pageTitle: "Videos"
-});
-export const upload = (req, res) => res.render("upload", {
-    pageTitle: "Upload"
-});
-export const videoDetail = (req, res) => res.render("videoDetail", {
-    pageTitle: "Video Deatail"
-});
-export const editVideo = (req, res) => res.render("editVideo", {
-    pageTitle: "Edit Video"
-});
-export const deleteVideo = (req, res) => res.render("deleteVideo", {
-    pageTitle: "Delete Video"
-});
+export const upload = (req, res) =>
+    res.render("upload", {
+        pageTitle: "Upload"
+    });
+export const videoDetail = (req, res) =>
+    res.render("videoDetail", {
+        pageTitle: "Video Detail"
+    });
+export const editVideo = (req, res) =>
+    res.render("editVideo", {
+        pageTitle: "Edit Video"
+    });
+export const deleteVideo = (req, res) =>
+    res.render("deleteVideo", {
+        pageTitle: "Delete Video"
+    });
