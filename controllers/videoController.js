25653936c9c89e88dfe015e49d27367e80a79662
sooyoungs0120@gmail.com <sooyoungs0120@gmail.com>
2019-05-21 21:45:08 +0900

    Delete Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -61,7 +61,7 @@ export const videoDetail = async (req, res) => {
     try {
         const video = await Video.findById(id);
         res.render("videoDetail", {
-            pageTitle: "Video Detail",
+            pageTitle: video.title,
             video
         });
     } catch (error) {
@@ -108,7 +108,19 @@ export const postEditVideo = async (req, res) => {
         res.redirect(routes.home);
     }
 };
-export const deleteVideo = (req, res) =>
-    res.render("deleteVideo", {
-        pageTitle: "Delete Video"
-    });
+export const deleteVideo = async (req, res) => {
+    const {
+        params: {
+            id
+        }
+    } = req;
+    try {
+        await Video.findOneAndRemove({
+            _id: id
+        });
+    } catch (error) {
+
+    }
+    res.redirect(routes.home);
+
+}
